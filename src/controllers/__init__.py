0100f3552ee7c8e0f181bf38b2708ef03bbdc5d7
danerlt <danerlt001@gmail.com>
2024-11-22 10:30:50 +0800

    style: Update controllers__init__.py

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -6,7 +6,7 @@ api_bp = Blueprint('api', __name__, url_prefix='/v1')
 api = ExternalApi(api_bp)
 
 # 导入接口相关模块，这里一定要写在 api 初始化后面，不然会报循环导入的错误
-from controlers.dataset import DatasetsApi, DatasetApi
+from controllers.dataset import DatasetsApi, DatasetApi
 
 # 设置 URL map
 api.add_resource(DatasetsApi, "/datasets")
